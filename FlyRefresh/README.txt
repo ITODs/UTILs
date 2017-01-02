d0be324891c9bae3e147f6ffceef0e0300b5f372
Binz-Chen <15712120787@163.com>
2017-01-02 11:09:42 +0800

    饼子FlyRefresh刷新

--- FlyRefresh/README.txt
+++ FlyRefresh/README.txt
@@ -1,3 +1,5 @@
 1.先必须引入开源包library里的开源代码
 2.其实引入安卓Api24 android-support-v7-recyclerview.jar包；
 3.在 build.gradle 引入开源 compile 'jp.wasabeef:recyclerview-animators:1.2.0@aar'
+
+详情请看源码解释：http://www.jcodecraeer.com/a/anzhuokaifa/androidkaifa/2015/0603/2987.html
--- FlyRefresh/app/src/main/java/com/example/flyrefresh/activity/MainActivity.java
+++ FlyRefresh/app/src/main/java/com/example/flyrefresh/activity/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -44,133 +45,137 @@ private LinearLayoutManager mLayoutManager;
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        /*初始化item数据*/
-        initDataSet();
-        setContentView(R.layout.activity_main);
+    super.onCreate(savedInstanceState);
+    /*初始化item数据*/
+    initDataSet();
+    setContentView(R.layout.activity_main);
+    /*适配标题栏*/
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayShowTitleEnabled(false);
 
 
-        mFlylayout = (FlyRefreshLayout) findViewById(R.id.fly_layout);
+    mFlylayout = (FlyRefreshLayout) findViewById(R.id.fly_layout);
 
-        mFlylayout.setOnPullRefreshListener(this);
+    mFlylayout.setOnPullRefreshListener(this);
 
-        mListView = (RecyclerView) findViewById(R.id.list);
+    mListView = (RecyclerView) findViewById(R.id.list);
 
-        mLayoutManager = new LinearLayoutManager(this);
-        mListView.setLayoutManager(mLayoutManager);
-        mAdapter = new ItemAdapter(this);
+    mLayoutManager = new LinearLayoutManager(this);
+    mListView.setLayoutManager(mLayoutManager);
+    mAdapter = new ItemAdapter(this);
 
-        mListView.setAdapter(mAdapter);
-        /*添加动画效果*/
-        mListView.setItemAnimator(new SampleItemAnimator());
-        }
+    mListView.setAdapter(mAdapter);
+    /*添加动画效果*/
+    mListView.setItemAnimator(new SampleItemAnimator());
+}
 
 private void initDataSet() {
-        mDataSet.add(new ItemData(Color.parseColor("#76A9FC"), R.mipmap.ic_assessment_white_24dp, "Meeting Minutes", new Date(2014 - 1900, 2, 9)));
-        mDataSet.add(new ItemData(Color.GRAY, R.mipmap.ic_folder_white_24dp, "Favorites Photos", new Date(2014 - 1900, 1, 3)));
-        mDataSet.add(new ItemData(Color.GRAY, R.mipmap.ic_folder_white_24dp, "Photos", new Date(2014 - 1900, 0, 9)));
-        }
+    mDataSet.add(new ItemData(Color.parseColor("#76A9FC"), R.mipmap.ic_assessment_white_24dp, "Meeting Minutes", new Date(2014 - 1900, 2, 9)));
+    mDataSet.add(new ItemData(Color.GRAY, R.mipmap.ic_folder_white_24dp, "Favorites Photos", new Date(2014 - 1900, 1, 3)));
+    mDataSet.add(new ItemData(Color.GRAY, R.mipmap.ic_folder_white_24dp, "Photos", new Date(2014 - 1900, 0, 9)));
+    }
 
 private void addItemData() {
-        ItemData itemData = new ItemData(Color.parseColor("#FFC970"), R.mipmap.ic_smartphone_white_24dp, "Magic Cube Show", new Date());
-        mDataSet.add(0, itemData);
-        mAdapter.notifyItemInserted(0);
-        mLayoutManager.scrollToPosition(0);
-        }
+    ItemData itemData = new ItemData(Color.parseColor("#FFC970"), R.mipmap.ic_smartphone_white_24dp, "Magic Cube Show", new Date());
+    mDataSet.add(0, itemData);
+    mAdapter.notifyItemInserted(0);
+    mLayoutManager.scrollToPosition(0);
+}
 
 @Override
 public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-        }
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+}
 
 @Override
 public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+    int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-        return true;
-        }
+    if (id == R.id.action_settings) {
+    return true;
+    }
 
-        return super.onOptionsItemSelected(item);
-        }
-/*刷新后页面调整*/
+    return super.onOptionsItemSelected(item);
+    }
+/*刷新后*/
 @Override
 public void onRefresh(FlyRefreshLayout view) {
-        View child = mListView.getChildAt(0);
-        if (child != null) {
+    View child = mListView.getChildAt(0);
+    if (child != null) {
         bounceAnimateView(child.findViewById(R.id.icon));
-        }
+    }
 
-        mHandler.postDelayed(new Runnable() {
-@Override
-public void run() {
-        mFlylayout.onRefreshFinish();
-        }
+    mHandler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+                mFlylayout.onRefreshFinish();
+            }
         }, 2000);
-        }
+}
 
 private void bounceAnimateView(View view) {
         if (view == null) {
-        return;
+            return;
         }
 
-        Animator swing = ObjectAnimator.ofFloat(view, "rotationX", 0, 30, -20, 0);
-        swing.setDuration(400);
-        swing.setInterpolator(new AccelerateInterpolator());
-        swing.start();
-        }
+    Animator swing = ObjectAnimator.ofFloat(view, "rotationX", 0, 30, -20, 0);
+    swing.setDuration(400);
+    swing.setInterpolator(new AccelerateInterpolator());
+    swing.start();
+}
 /*刷新后添加数据*/
 @Override
 public void onRefreshAnimationEnd(FlyRefreshLayout view) {
-        addItemData();
-        }
+    addItemData();
+}
 /*定义ItemAdapter适配器*/
 private class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
 
-    private LayoutInflater mInflater;
-    private DateFormat dateFormat;
+private LayoutInflater mInflater;
+private DateFormat dateFormat;
 
-    public ItemAdapter(Context context) {
-        mInflater = LayoutInflater.from(context);
-        dateFormat = SimpleDateFormat.getDateInstance(DateFormat.DEFAULT, Locale.ENGLISH);
-    }
+public ItemAdapter(Context context) {
+    mInflater = LayoutInflater.from(context);
+    dateFormat = SimpleDateFormat.getDateInstance(DateFormat.DEFAULT, Locale.ENGLISH);
+}
 
-    @Override
-    public ItemViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view = mInflater.inflate(R.layout.view_list_item, viewGroup, false);
-        return new ItemViewHolder(view);
-    }
+@Override
+public ItemViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    View view = mInflater.inflate(R.layout.view_list_item, viewGroup, false);
+    return new ItemViewHolder(view);
+}
 
-    @Override
-    public void onBindViewHolder(ItemViewHolder itemViewHolder, int i) {
-        final ItemData data = mDataSet.get(i);
-        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
-        drawable.getPaint().setColor(data.color);
-        itemViewHolder.icon.setBackgroundDrawable(drawable);
-        itemViewHolder.icon.setImageResource(data.icon);
-        itemViewHolder.title.setText(data.title);
-        itemViewHolder.subTitle.setText(dateFormat.format(data.time));
-    }
+@Override
+public void onBindViewHolder(ItemViewHolder itemViewHolder, int i) {
+    final ItemData data = mDataSet.get(i);
+    ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
+    drawable.getPaint().setColor(data.color);
+    itemViewHolder.icon.setBackgroundDrawable(drawable);
+    itemViewHolder.icon.setImageResource(data.icon);
+    itemViewHolder.title.setText(data.title);
+    itemViewHolder.subTitle.setText(dateFormat.format(data.time));
+}
 
-    @Override
-    public int getItemCount() {
-        return mDataSet.size();
-    }
+@Override
+public int getItemCount() {
+    return mDataSet.size();
+}
 }
 /*定义缓存的viewHolder 模型*/
 private static class ItemViewHolder extends RecyclerView.ViewHolder {
 
-    ImageView icon;
-    TextView title;
-    TextView subTitle;
+ImageView icon;
+TextView title;
+TextView subTitle;
 
-    public ItemViewHolder(View itemView) {
-        super(itemView);
-        icon = (ImageView) itemView.findViewById(R.id.icon);
-        title = (TextView) itemView.findViewById(R.id.title);
-        subTitle = (TextView) itemView.findViewById(R.id.subtitle);
-    }
+public ItemViewHolder(View itemView) {
+    super(itemView);
+    icon = (ImageView) itemView.findViewById(R.id.icon);
+    title = (TextView) itemView.findViewById(R.id.title);
+    subTitle = (TextView) itemView.findViewById(R.id.subtitle);
+}
 
 }
 }
